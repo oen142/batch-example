bde05f856abd806973a037bf57b8b427a9efc90a
oen142 <oen142@naver.com>
2021-09-28 01:09:11 +0900

    Initial commit

---
+++ src/main/kotlin/fastcampus/spring/batch/part1/HelloConfiguration.kt
@@ -0,0 +1,38 @@
+package fastcampus.spring.batch.part1
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class HelloConfiguration(
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val stepBuilderFactory: StepBuilderFactory
+) {
+
+    @Bean
+    fun helloJob(): Job {
+        return jobBuilderFactory.get("helloJob")
+            .incrementer(RunIdIncrementer())
+            .start(this.helloStep())
+            .build()
+    }
+
+    @Bean
+    fun helloStep(): Step {
+        return stepBuilderFactory["helloStep"]
+            .tasklet { contribution: StepContribution, chunkContext: ChunkContext? ->
+                println("contribution = $contribution")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+}
